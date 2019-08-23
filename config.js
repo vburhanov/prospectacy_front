931e9478c2b582f93aa1ed88abd60bfd73d3f4fa
vburhanov <vburhanov@yandex.ru>
2019-08-23 12:24:41 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,46 @@
+/* global module */
+
+let config = {
+  'notGetBlocks': [
+    'blocks-demo.html',
+  ],
+  'ignoredBlocks': [
+    'no-js',
+  ],
+  'alwaysAddBlocks': [
+    'main-nav'
+    // 'sprite-svg',
+    // 'sprite-png',
+    // 'object-fit-polyfill',
+  ],
+  'addStyleBefore': [
+    'src/scss/variables.scss',
+    'src/scss/mixins.scss',
+    'src/scss/main.scss'
+    // 'somePackage/dist/somePackage.css', // для 'node_modules/somePackage/dist/somePackage.css',
+  ],
+  'addStyleAfter': [
+    // 'src/scss/print.scss',
+  ],
+  'addJsBefore': [
+    // 'somePackage/dist/somePackage.js', // для 'node_modules/somePackage/dist/somePackage.js',
+  ],
+  'addJsAfter': [
+    './script.js',
+  ],
+  'addAssets': {
+    'src/img/*.{png,svg,jpg,jpeg}': 'img/',
+    'src/fonts/graphik/GraphikLCG-Medium.otf': 'fonts/',
+    'src/fonts/graphik/GraphikLCG-Regular.otf': 'fonts/',
+    'src/fonts/halvar/HalvarBreitschriftRegular.ttf': 'fonts/'
+    // 'src/favicon/*.{png,ico,svg,xml,webmanifest}': 'img/favicon',
+    // 'node_modules/somePackage/images/*.{png,svg,jpg,jpeg}': 'img/',
+  },
+  'dir': {
+    'src': 'src/',
+    'build': 'build/',
+    'blocks': 'src/blocks/'
+  }
+};
+
+module.exports = config;
